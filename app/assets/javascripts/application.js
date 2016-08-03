1663b73902ecf8621414e21782523042e4eab1d9
Dominic <dom42486@gmail.com>
2016-08-03 01:26:53 -0700

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,50 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require bootstrap.min
+//= require admin-lte
+//= require turbolinks
+//= require_tree .
+
+//function - toggle the sidebar when hamburger button is clicked
+$(function(){
+  var isOpen = false;
+  $('#hamburger').click(function(){
+    if(isOpen == false){
+      $('#sidebar').animate({
+        width: '250px'
+      }, 500);
+      this.blur();
+      isOpen = true;
+    }
+    else {
+      $('#sidebar').animate({
+        width: '0px'
+      }, 250);
+      this.blur();
+      isOpen = false;
+    }
+    return isOpen;
+  });
+});
+
+// function - show password fields when 'Change Password' link is clicked
+$(function(){
+  $('#show-fields').click(function(e){
+    e.preventDefault();
+    $('#hidden-fields').removeClass('hidden');
+    $('#changePW').addClass('hidden');
+  });
+});
